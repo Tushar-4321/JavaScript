7c8abb0ec388dfd3d5d7f22dfcee997f8f370fd0
Tushar-4321 <110771715+Tushar-4321@users.noreply.github.com>
2026-02-05 06:37:27 +0000

    dataTypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+/*JS few years(ex = 8yrs)back was different it didn't had class, modules, arrow function but new Js had it, so JS Community or organization didn't want their old code to be useless as we are new code as many things were already written in old format, and when we are writing new code, it should be treated via new code standard,
+as all gonna run in same engine
+SO FOR THIS WE USE == "use strict";  // it will treat all code as newer version of javascript
+*/
+"use strict";//treat all code as newer version
+//alert(3 + 3 ) //we are using nodejs not browser, allowed in browser it shows 6
+
+console.log(3+3)
+// always try to make code readability high
+
+let name = "Tushar"//string datatype
+let age = 25 //number
+let iSLoggedIn = true // boolean
+
+/* various dataTypes of JS are 
+number --> range to 2 to power of 53
+bigint  -- for large number
+string ==> ""
+boolean ==> true/false
+null ==> standalone value(empty)
+undefined ==> value not assigned
+symbol ==> uniquness
+
+object
+*/
+
+console.log(typeof null);// null is object (shown in output) not a type// but comes in category of dataType
