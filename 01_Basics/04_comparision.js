54a5a16129fc7fec1a6142ca3ccaf535e592cbd2
Tushar-4321 <110771715+Tushar-4321@users.noreply.github.com>
2026-02-06 08:43:23 +0000

    comparisons

--- 01_Basics/04_comparision.js
+++ 01_Basics/04_comparision.js
@@ -1,1 +1,25 @@
-console.log();
+//console.log(2>1); output in boolean ==> true
+
+// console.log("2">1);
+// console.log("02">1);// here js automatically converted the "2" in number
+ 
+// console.log(null>0); // ==> false  as null is empty
+// console.log(null == 0); // ==> false
+// console.log(null >= 0); // ==> true
+// console.log(null <= 0); // ==> true
+// console.log(null < 0); // ==> false
+
+/* The reason is that an equality check == and comparisons < > >= <= work differently.
+Comparisons convert null to a number, treating it as 0.
+That's why(3) null >= 0 is true and (1) null > 0 is false. */
+
+// console.log(undefined == 0);
+// console.log(undefined < 0);
+// console.log(undefined > 0);
+// console.log(undefined >= 0);  all these undefined are false
+
+// Strict check ==> "===" this not only checks it's value but also it's datatype
+
+console.log("2" === 2); // ==> false
+
+
