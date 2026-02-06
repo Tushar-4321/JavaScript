79cba6eca5f181e46daa093f924fba69a4053275
Tushar-4321 <110771715+Tushar-4321@users.noreply.github.com>
2026-02-06 09:16:45 +0000

    datatype-summary

---
+++ 01_Basics/datatypes-summary.js
@@ -0,0 +1,9 @@
+// in official documentation data is categorised in only two types 
+// ===-> Primitive
+// ===-> non - primitive
+// means how data is stored and how we access data on this basis 2 categories are created
+
+// ==> # Primitive --> whenever we copy or call it from somewhere we only get it's copied data not the refrence of the memory where original data is stored and the changes we do is only reflected in the copied the data not the original untill and unless we specifically change the original data(({[CALL BY VALUE]})). 
+    //  == > string, NUMBER, boolean, null(empty), undefined, symbol(it is used to make a value unique), BigInt 
+
+// ==     
