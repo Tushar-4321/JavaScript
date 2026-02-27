ca5d2aefc39d5363eb8b3bf58c5547d69f038d24
Tushar-4321 <110771715+Tushar-4321@users.noreply.github.com>
2026-02-27 08:51:37 +0000

    num and math

---
+++ 01_Basics/06_nums_and_maths.js
@@ -0,0 +1,10 @@
+// node 01_Basics/06_nums_and_maths.js
+
+const score = 400 // here it automatically takes 400 as number in javascript 
+const balance = new Number(600) // here it mentions in the output that 600 is number
+console.log(score);
+
+console.log(balance);
+console.log(typeof balance);
+console.log(balance.toFixed(3));
+
